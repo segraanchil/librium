4b35ecfd94e9961e5026f442ce2f7f48a9d590cf
segraanchil <120653806+segraanchil@users.noreply.github.com>
2025-02-10 22:10:11 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const modal = document.getElementById("letterModal");
+    const btn = document.getElementById("openLetterBtn");
+    const span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
